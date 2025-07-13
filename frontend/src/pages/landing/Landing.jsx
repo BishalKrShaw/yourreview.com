c663ee1290657ad24db8dbeb229f5c9e4cfa608d
BishalKrShaw <bishalshaw.mfis@gmail.com>
2025-07-14 01:02:44 +0530

    LandingPage changes

--- frontend/src/pages/landing/Landing.jsx
+++ frontend/src/pages/landing/Landing.jsx
@@ -1,45 +1,17 @@
-// import React from "react";
-// import { useNavigate } from "react-router-dom";
-// import Navbar from "../../components/Navbar";
-// import Footer from "../../components/Footer";
-
-// const Landing = () => {
-//   const navigate = useNavigate();
-
-//   return (
-//     <>
-//       <div className="bg-black">
-//         <Navbar />
-//         <div className="px-5 py-4 h-screen flex justify-center items-center text-center font-normal flex-col leading-tight w-[1000px] mx-auto text-white">
-//           <div className="text-[3rem]">
-//             Collect reviews from your customers &
-//           </div>
-//           <div className="text-[3rem]">display in your website easily.</div>
-//           <button
-//             className="px-6 py-2 rounded-full bg-white hover:bg-white/90 text-black border border-white/10 shadow-[0_4px_20px_rgba(255,255,255,0.1)] transition duration-200 cursor-pointer mt-10 text-[1.3rem] backdrop-blur-sm"
-//             onClick={() => navigate("/signup")}
-//           >
-//             Get started
-//           </button>
-//         </div>
-//       </div>
-//       <Footer />
-//     </>
-//   );
-// };
-
-// export default Landing;
 
 import React, { useRef, useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
+import { useNavigate } from "react-router-dom";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Landing = () => {
   const sectionsRef = useRef([]);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     sectionsRef.current.forEach((section) => {
@@ -121,7 +93,7 @@ const Landing = () => {
           Register and start collecting reviews instantly. No credit card required. Perfect for testing and building trust.
         </p>
         <button
-          onClick={() => window.location.href = "/signup"}
+          onClick={() => navigate("/signup")}
           className="px-6 py-3 rounded-full bg-white text-black hover:bg-white/90 shadow transition text-lg cursor-pointer"
         >
           Sign Up Now →
