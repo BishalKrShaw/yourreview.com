8570e9a91828aaacdf809c2660014cc5895f046b
BishalKrShaw <bishalshaw.mfis@gmail.com>
2025-07-14 00:49:28 +0530

    main.jsx changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -21,7 +21,7 @@ import CreateCampaign from "./pages/campaign/CreateCampaign.jsx";
 import Profile from "./pages/profile/Profile.jsx";
 import Services from "./pages/Services.jsx";
 import AboutUs from "./pages/AboutUs.jsx";
-import ContactUs from "./pages/Contactus.jsx";
+import ContactUs from "./pages/ContactUs.jsx";
 import PrivacyPolicy from "./pages/PrivayPolicy.jsx";
 import Pricing from "./pages/Pricing.jsx";
 import TermsAndConditions from "./pages/TermsAndConditions.jsx";
